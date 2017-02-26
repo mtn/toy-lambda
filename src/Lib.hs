32e1104aa300b70571ec28f626c36f6993e2c694
Michael Noronha <michaeltnoronha@gmail.com>
2017-02-26 01:17:15 -0600

    Removed unicode and started adding let-statements

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE UnicodeSyntax #-}
-
 module Lib where
 
 import AbsLambda
@@ -12,27 +10,28 @@ data Term
     = TmVar String Integer
     | TmInt Integer
     | TmApp Term Term
+    | TmLet Ident Term Term
     | TmAbs Ident Term
     | TmAdd Term Term
   deriving (Eq, Ord, Show, Read)
 
-rmNames :: Context → NmTerm → Term
+rmNames :: Context -> NmTerm -> Term
 rmNames c (NmTmVar (Ident str))    = case (elemIndex (str,NameBind) c) of
-                                        Just a  → TmVar str $ fromIntegral a
-                                        Nothing → error "Undefined"
+                                        Just a  -> TmVar str $ fromIntegral a
+                                        Nothing -> error "Undefined"
 rmNames _ (NmTmInt n)              = TmInt $ (fromIntegral n)
 rmNames c (NmTmApp tm1 tm2)        = TmApp (rmNames c tm1) (rmNames c tm2)
 rmNames c (NmTmAbs (Ident str) tm) = let c' = (str,NameBind):c in TmAbs (Ident str) $ rmNames c' tm
 rmNames c (NmTmAdd tm1 tm2)        = TmAdd (rmNames c tm1) (rmNames c tm2)
 
-prettyTerm :: Term → String
+prettyTerm :: Term -> String
 prettyTerm (TmVar str n)          = "x@" ++ show n
 prettyTerm (TmInt n)              = show n
 prettyTerm (TmApp tm1 tm2)        = (prettyTerm tm1) ++ " " ++ (prettyTerm tm2)
 prettyTerm (TmAbs str tm)         = "λ " ++ prettyTerm tm
 prettyTerm (TmAdd tm1 tm2)        = (prettyTerm tm1) ++ " + " ++  (prettyTerm tm2)
 
-shift :: Integer → Term → Term
+shift :: Integer -> Term -> Term
 shift n tm =
     let walk c (TmVar str n1)
           | n1 >= c   = TmVar str (n1 + n)
@@ -43,15 +42,15 @@ shift n tm =
         walk _ x               = x
     in walk 0 tm
 
-subst' ∷ Integer → Term → Term → Term
+subst' :: Integer -> Term -> Term -> Term
 subst' j s t =
   let walk c term
         = case term of
-            TmVar str x → if x == j + c
-                          then shift c s
-                          else TmVar str x
-            TmAbs x t₁ → TmAbs x (walk (c + 1) t₁)
-            TmApp t₁ t₂ → TmApp (walk c t₁) (walk c t₂)
+            TmVar str x   -> if x == j + c
+                            then shift c s
+                            else TmVar str x
+            TmAbs x t1    -> TmAbs x (walk (c + 1) t1)
+            TmApp t1 t2   -> TmApp (walk c t1) (walk c t2)
             TmAdd tm1 tm2 -> TmAdd (walk c tm1) (walk c tm2)
             tm            -> tm
   in walk 0 t
